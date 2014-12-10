d02ba983ffbdfcb6ae74ac64f75d599f4f0c2ebe
Alex <220619@gmail.com>
2014-12-10 18:09:15 +0300

    Get locations data

--- app/controllers/ParserController.php
+++ app/controllers/ParserController.php
@@ -23,64 +23,6 @@ class ParserController extends BaseController
         }
     }
 
-    public function jAction(){
-        echo 1;
-    }
-
-    public function curlAction()
-    {
-        $baseUrl = 'http://booking.com';
-
-        $provider  = Request::getProvider(); // get available provider Curl or Stream
-
-        $provider->setBaseUri($baseUrl);
-
-        $provider->header->set('Accept', 'application/json');
-
-//        $provider->header->set('Accept', 'text/html');
-//        $provider->header->set('Accept', 'application/xhtml+xml');
-//        $provider->header->set('Accept', 'application/xml');
-
-        $params = array(
-            'lang'=>'ru',
-            'pid'=>'6b1c422f5a320072',
-            'sid'=>'02da8e0f4b98680e70edc35ac6b45492',
-            'aid'=>304142,
-            'stype'=>1,
-            'force_ufi'=>'',
-            'cities_first'=>1,
-            'should_split'=>1,
-            'sugv'=>'br',
-            'e_acb1'=>1,
-            'e_acb2'=>1,
-            'eb'=>0,
-            'add_themes'=>1,
-            'themes_match_start'=>0,
-            'include_synonyms'=>1,
-            'e_nr_labels'=>1,
-            'e_obj_labels'=>1,
-            'exclude_some_hotels'=>1,
-            'include_dest_count'=>0,
-            'max_results'=>'10',
-            'include_extra_synonyms'=>0,
-            'term'=>'Rome',
-        );
-
-        $provider->setOption(CURLOPT_SSL_VERIFYPEER, false);
-
-        $response = $provider->get('/', $params);
-
-        echo $response->header->status;
-
-        print_r($response);
-        //echo $response->body;
-
-//        $uri = $provider->resolveUri($baseUrl.'/autocomplete');
-//        $uri->extendQuery($params);
-//        echo $uri->build();
-
-    }
-
 
     // curl -i
     // -H "User-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36"
@@ -91,44 +33,22 @@ class ParserController extends BaseController
     // -H "Cookie: utag_main=v_id:0149e65771d7001ecb26216f6ca605066003705e00bd0$_sn:1$_ss:1$_pn:1%3Bexp-session$_st:1416910530839$ses_id:1416908730839%3Bexp-session; zz_cook_segment=1; _ga=GA1.2.272112262.1416908731; __utma=1.272112262.1416908731.1416908731.1416908731.1; __utmc=1; __utmz=1.1416908731.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); bkng=11UmFuZG9tSVYkc2RlIyh9YWJdm48m5cJDWuLLIYaigN7j6j48cUdQU8ks0%2Fk62aC%2BxdDf7fqoRnTtnrQ7rtxNgi9Nffvg1Sj17HjkVCAzuJbqR8QXty4azG%2BIzhFHqNRBFT1J1B8B%2FYIgcqxushhAUAh2A4WabiSsfZRFPR2tf2qTMKNI0dulkCB53sH2MK8L149VfRyORNE%3D"
     // -X GET 'http://booking.com/autocomplete?lang=ru&pid=6b1c422f5a320072&sid=02da8e0f4b98680e70edc35ac6b45492&aid=304142&stype=1&force_ufi=&cities_first=1&should_split=1&sugv=br&e_acb1=1&e_acb2=1&eb=0&add_themes=1&themes_match_start=0&include_synonyms=1&e_nr_labels=1&e_obj_labels=1&exclude_some_hotels=1&include_dest_count=1&max_results=10&include_extra_synonyms=0&term=Italy'
 
-    public function curl1Action(){
-        $url = 'http://booking.com/autocomplete?_=1417439491177&lang=ru&pid=54545cb3cc8300fa&sid=02da8e0f4b98680e70edc35ac6b45492&aid=304142&stype=1&force_ufi=&cities_first=1&should_split=1&sugv=br&e_acb1=1&eb=1&add_themes=1&sort_nr_destinations=1&themes_match_start=1&include_synonyms=1&e_nr_labels=1&e_obj_labels=1&exclude_some_hotels=&term=moscow';
+    private function getLocationJSON($location = ''){
 
-        $headers = array(
-            "User-Agent"=>"Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36",
-            "Content-Type"=>"text/plain; charset=utf-8",
-            "Accept"=>"*/*",
-            "Accept-Encoding"=>"gzip, deflate, sdch",
-            "Accept-Language"=>"en-US,en;q=0.8,ru;q=0.6",
-            "Cookie"=>'utag_main=v_id:0149e65771d7001ecb26216f6ca605066003705e00bd0$_sn:1$_ss:1$_pn:1%3Bexp-session$_st:1416910530839$ses_id:1416908730839%3Bexp-session; zz_cook_segment=1; _ga=GA1.2.272112262.1416908731; __utma=1.272112262.1416908731.1416908731.1416908731.1; __utmc=1; __utmz=1.1416908731.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); bkng=11UmFuZG9tSVYkc2RlIyh9YWJdm48m5cJDWuLLIYaigN7j6j48cUdQU8ks0%2Fk62aC%2BxdDf7fqoRnTtnrQ7rtxNgi9Nffvg1Sj17HjkVCAzuJbqR8QXty4azG%2BIzhFHqNRBFT1J1B8B%2FYIgcqxushhAUAh2A4WabiSsfZRFPR2tf2qTMKNI0dulkCB53sH2MK8L149VfRyORNE%3D',
-        );
-
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_VERBOSE, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_URL, $url);
-
-        $result = curl_exec($ch);
-
-        var_dump($result);
-    }
-
-    public function curl2Action(){
-
-        $location = $this->request->get('location');
-
-        echo '<h1>'.$location.'</h1>';
+        if(empty($location)) $location = $this->request->get('location');
 
 $bashCommand = <<<EOD
 curl -H "User-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36" -H "Content-Type: text/plain; charset=utf-8" -H "Accept: application/json" -H "Accept-Encoding: gzip, deflate, sdch" -H "Accept-Language: en-US,en;q=0.8,ru;q=0.6" -X GET 'http://booking.com/autocomplete?lang=ru&pid=6b1c422f5a320072&sid=02da8e0f4b98680e70edc35ac6b45492&aid=304142&stype=1&force_ufi=&cities_first=1&should_split=1&sugv=br&e_acb1=1&e_acb2=1&eb=0&add_themes=1&themes_match_start=0&include_synonyms=1&e_nr_labels=1&e_obj_labels=1&exclude_some_hotels=1&include_dest_count=1&max_results=10&include_extra_synonyms=0&term=$location'
 EOD;
-        $stdOut = array();
 
         $output = shell_exec($bashCommand);
+
+        return $output;
+
         $output = json_decode($output);
+
+        $stdOut = array();
+
         if(!empty($output->city)){
             foreach($output->city as $set){
                 /*
@@ -168,14 +88,63 @@ EOD;
                 [nr_hotels_label] => 1386 вариантов размещения
                 [rtl] => 0
                 [type] => ci
-                 */
+                */
                 $stdOut[] = $set->label_highlighted;
-                $stdOut[] = '<a href="http://www.booking.com/searchresults.ru.html?sid=02da8e0f4b98680e70edc35ac6b45492;dcid=1;checkin=2014-12-12;checkout=2014-12-13;dest_id='.$set->dest_id.';dest_type=city;ilp=1" target="_blank">'.$set->nr_hotels_label.'</a>';
+                $stdOut[] = '<a href="http://booking.com/searchresults.ru.html?city='.$set->dest_id.'" target="_blank">'.$set->nr_hotels_label.'</a>';
                 $stdOut[] = '';
 
 
             }
+
+            echo '<h1>'.$location.'</h1>';
+
             echo implode('<br />', $stdOut);
         }
+    }
+
+    public function getCityJSONAction(){
+
+        $cities = new Cities();
+
+        $result = $cities->query()->where('json IS NULL')->andWhere('country_id = 1')->limit(1)->execute();
+
+        if($city = $result->getFirst()){
+            echo $city->getCityId().': '.$city->getTitleEn();
+
+            $jsonStr = $this->getLocationJSON($city->getTitleEn());
+
+            $city->setJson($jsonStr);
+
+            if($city->save()){
+                echo ' OK';
+            }
+        }else{
+            echo 'No data';
+        }
+
+
+        $this->view->disable();
+    }
+
+    public function _curlAction()
+    {
+        $baseUrl = 'http://booking.com';
+
+        $provider  = Request::getProvider();
+
+        $provider->setBaseUri($baseUrl);
+
+        $provider->header->set('Accept', 'application/json');
+
+        $provider->setOption(CURLOPT_SSL_VERIFYPEER, false);
+
+        $params = array();
+
+        $response = $provider->get('/', $params);
+
+        echo $response->header->status;
+
+        //echo $response->body;
+
     }
 }
---
+++ app/models/Cities.php
@@ -0,0 +1,360 @@
+<?php
+
+class Cities extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    protected $city_id;
+
+    /**
+     *
+     * @var integer
+     */
+    protected $country_id;
+
+    /**
+     *
+     * @var integer
+     */
+    protected $important;
+
+    /**
+     *
+     * @var integer
+     */
+    protected $region_id;
+
+    /**
+     *
+     * @var string
+     */
+    protected $title_ru;
+
+    /**
+     *
+     * @var string
+     */
+    protected $area_ru;
+
+    /**
+     *
+     * @var string
+     */
+    protected $region_ru;
+
+    /**
+     *
+     * @var string
+     */
+    protected $title_en;
+
+    /**
+     *
+     * @var string
+     */
+    protected $area_en;
+
+    /**
+     *
+     * @var string
+     */
+    protected $region_en;
+
+    /**
+     *
+     * @var string
+     */
+    protected $json;
+
+    protected $http_status;
+
+    /**
+     * Method to set the value of field city_id
+     *
+     * @param integer $city_id
+     * @return $this
+     */
+    public function setCityId($city_id)
+    {
+        $this->city_id = $city_id;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field country_id
+     *
+     * @param integer $country_id
+     * @return $this
+     */
+    public function setCountryId($country_id)
+    {
+        $this->country_id = $country_id;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field important
+     *
+     * @param integer $important
+     * @return $this
+     */
+    public function setImportant($important)
+    {
+        $this->important = $important;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field region_id
+     *
+     * @param integer $region_id
+     * @return $this
+     */
+    public function setRegionId($region_id)
+    {
+        $this->region_id = $region_id;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field title_ru
+     *
+     * @param string $title_ru
+     * @return $this
+     */
+    public function setTitleRu($title_ru)
+    {
+        $this->title_ru = $title_ru;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field area_ru
+     *
+     * @param string $area_ru
+     * @return $this
+     */
+    public function setAreaRu($area_ru)
+    {
+        $this->area_ru = $area_ru;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field region_ru
+     *
+     * @param string $region_ru
+     * @return $this
+     */
+    public function setRegionRu($region_ru)
+    {
+        $this->region_ru = $region_ru;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field title_en
+     *
+     * @param string $title_en
+     * @return $this
+     */
+    public function setTitleEn($title_en)
+    {
+        $this->title_en = $title_en;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field area_en
+     *
+     * @param string $area_en
+     * @return $this
+     */
+    public function setAreaEn($area_en)
+    {
+        $this->area_en = $area_en;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field region_en
+     *
+     * @param string $region_en
+     * @return $this
+     */
+    public function setRegionEn($region_en)
+    {
+        $this->region_en = $region_en;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field json
+     *
+     * @param string $json
+     * @return $this
+     */
+    public function setJson($json)
+    {
+        $this->json = $json;
+
+        return $this;
+    }
+
+    public function setHTTPStatus($httpStatus = 0)
+    {
+        $this->http_status = $httpStatus;
+
+        return $this;
+    }
+
+    /**
+     * Returns the value of field city_id
+     *
+     * @return integer
+     */
+    public function getCityId()
+    {
+        return $this->city_id;
+    }
+
+    /**
+     * Returns the value of field country_id
+     *
+     * @return integer
+     */
+    public function getCountryId()
+    {
+        return $this->country_id;
+    }
+
+    /**
+     * Returns the value of field important
+     *
+     * @return integer
+     */
+    public function getImportant()
+    {
+        return $this->important;
+    }
+
+    /**
+     * Returns the value of field region_id
+     *
+     * @return integer
+     */
+    public function getRegionId()
+    {
+        return $this->region_id;
+    }
+
+    /**
+     * Returns the value of field title_ru
+     *
+     * @return string
+     */
+    public function getTitleRu()
+    {
+        return $this->title_ru;
+    }
+
+    /**
+     * Returns the value of field area_ru
+     *
+     * @return string
+     */
+    public function getAreaRu()
+    {
+        return $this->area_ru;
+    }
+
+    /**
+     * Returns the value of field region_ru
+     *
+     * @return string
+     */
+    public function getRegionRu()
+    {
+        return $this->region_ru;
+    }
+
+    /**
+     * Returns the value of field title_en
+     *
+     * @return string
+     */
+    public function getTitleEn()
+    {
+        return $this->title_en;
+    }
+
+    /**
+     * Returns the value of field area_en
+     *
+     * @return string
+     */
+    public function getAreaEn()
+    {
+        return $this->area_en;
+    }
+
+    /**
+     * Returns the value of field region_en
+     *
+     * @return string
+     */
+    public function getRegionEn()
+    {
+        return $this->region_en;
+    }
+
+    /**
+     * Returns the value of field json
+     *
+     * @return string
+     */
+    public function getJson()
+    {
+        return $this->json;
+    }
+
+    public function getHTTPStatus()
+    {
+        return $this->http_status;
+    }
+
+    /**
+     * Independent Column Mapping.
+     */
+    public function columnMap()
+    {
+        return array(
+            'city_id' => 'city_id', 
+            'country_id' => 'country_id', 
+            'important' => 'important', 
+            'region_id' => 'region_id', 
+            'title_ru' => 'title_ru', 
+            'area_ru' => 'area_ru', 
+            'region_ru' => 'region_ru', 
+            'title_en' => 'title_en', 
+            'area_en' => 'area_en', 
+            'region_en' => 'region_en', 
+            'json' => 'json',
+            'http_status' => 'http_status',
+        );
+    }
+
+}
