5c866c51ff69cb3fc92b0bee968fb3e1877f3851
Alex <220619@gmail.com>
2014-12-18 19:53:24 +0300

    get hotel action created

--- app/controllers/ParserController.php
+++ app/controllers/ParserController.php
@@ -215,12 +215,10 @@ EOD;
             $location = $result->getUrlOrig();
 
             $bashCommand = <<<EOD
-curl -H "User-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36" -H "Content-Type: text/plain; charset=utf-8" -H "Accept: text/plain" -H "Accept-Encoding: gzip, deflate, sdch" -H "Accept-Language: en-US,en;q=0.8,ru;q=0.6" -X -L GET 'http://booking.com$location'
+curl -H "User-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36" -H "Content-Type: text/plain; charset=utf-8" -H "Accept: text/plain" -H "Accept-Encoding: gzip, deflate, sdch" -H "Accept-Language: en-US,en;q=0.8,ru;q=0.6" -X GET -L 'http://booking.com$location'
 EOD;
 
-            echo $bashCommand;
-
-            var_dump(shell_exec($bashCommand));
+            echo shell_exec($bashCommand);
 
         }catch (\Phalcon\Exception $e){
             echo 'Empty<br >' . PHP_EOL;
