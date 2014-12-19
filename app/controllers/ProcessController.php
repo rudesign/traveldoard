0173680dcb98aabc83d89e820bb445c844bd3b63
Alex <220619@gmail.com>
2014-12-19 19:21:01 +0300

    process hotel info created

--- app/controllers/ProcessController.php
+++ app/controllers/ProcessController.php
@@ -206,17 +206,29 @@ class ProcessController extends BaseController
                         if ($rawEntry = explode('.', $entry)) $hotelId = (int) reset($rawEntry);
 
                         $hotels = new Hotels();
-                        if(!$hotel = $hotels->query()->where('status=202')->execute()->getFirst()) throw new PException('No data');
+                        if(!$hotel = $hotels->query()->where('hotel_id='.$hotelId)->execute()->getFirst()) throw new PException('No data');
 
-                        echo 'Processed '.$hotel->getName().', '.$hotel->cities->getTitleRu().'<br />' . PHP_EOL;
+                        $qp = htmlqp($fname, '.wrap-hotelpage-top');
 
-                        $qp = htmlqp($fname, 'body');
+                        if(!$qp->count()) throw new PException('No html slice');
 
-                        if($qp->count()){
+//                        <span id="hp_address_subtitle" class="jq_tooltip" rel="14" data-source="top_link" data-coords="37.610327093193064,55.75103368673392" data-node_tt_id="location_score_tooltip" data-bbox="37.58050561,55.74550902,37.62325346,55.76068242" data-width="350" title="">
+//                            Улица Моховая 10, Здание 1, Москва, Россия,
+//                        </span>
 
-                        } else echo 'No html slice<br />' . PHP_EOL;
+                        $address = trim($qp->find('#hp_address_subtitle')->text());
+
+                        if(!empty($address)){
+                            $hotel->setAddress($address);
+                            echo 'Address: '.$address.'<br />' . PHP_EOL;
+                        }
+
+                        $hotel->save();
 
                         //if(rename($fname, $newFname)) echo 'Source file renamed'; else echo 'Source file rename failed';
+
+                        echo 'Processed <a href="/rawHotels/items/'.$hotel->getHotelId().'.html" target="_blank">'.$hotel->getName().'</a>, '.$hotel->cities->getTitleRu().'<br />' . PHP_EOL;
+
 
                         break;
                     }
