c75acf15ec68931a6f1c3d77f2181e5d184d43e0
Alex <220619@gmail.com>
2015-05-08 19:11:35 +0300

    Sabre auth added

--- app/controllers/SabreController.php
+++ app/controllers/SabreController.php
@@ -8,6 +8,8 @@ class SabreController extends BaseController
 
     public function initialize(){
         parent::initialize();
+
+        $this->getToken();
     }
 
     public function ShowAction($type= '')
@@ -16,7 +18,9 @@ class SabreController extends BaseController
 
         switch($type){
             case 'flights':
-                $this->GetFights();
+                $this->getFights();
+                $this->getMACs();
+                $this->showFlights();
             break;
         }
 
@@ -25,7 +29,24 @@ class SabreController extends BaseController
         $this->view->disable();
     }
 
-    protected function GetFights()
+    public function SourceAction($type= '')
+    {
+        $data = array();
+
+        switch($type){
+            case 'flights':
+                $this->getFights();
+                $this->data['html'] = '';
+                $this->showTree($this->data['flights']);
+            break;
+        }
+
+        echo json_encode($this->data);
+
+        $this->view->disable();
+    }
+
+    protected function getFights()
     {
         $data = array();
 
@@ -54,26 +75,36 @@ class SabreController extends BaseController
         }
         */
 
-        $response = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'].'/flight'), true);
-
-        $this->data['html'] = '';
-        $this->showTree($response);
-
-        /*
-        foreach($response->PricedItineraries as $itinerary){
-
-            //$set['fare'] = $itinerary->AirItineraryPricingInfo->ItinTotalFare->TotalFare->Amount;
-
-            var_dump(get_object_vars($itinerary->AirItinerary->OriginDestinationOptions));//->OriginDestinationOption);
-            die;
-
-            $set['ElapsedTime'] = $itinerary->AirItinerary;
-
-            //$data['itineraries'][] = $set;
-        }
-        */
+        $this->data['flights'] = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'].'/flight'), true);
 
         return true;
+    }
+
+    protected function showFlights(){
+        if(!empty($this->data['flights'])){
+            $this->data['html'] = '';
+            foreach($this->data['flights']['PricedItineraries'] as $itinerary){
+                $airItinerary = $itinerary['AirItinerary'];
+                $options = $airItinerary['OriginDestinationOptions']['OriginDestinationOption'];
+                $this->data['html'] .= '<div>';
+                foreach($options as $option){
+                    $segments = $option['FlightSegment'];
+                    $this->data['html'] .= '<div class="leg">';
+                    foreach($segments as $segment){
+                        $departureAirportMAC = $segment['DepartureAirport']['LocationCode'];
+                        $arrivalAirportMAC = $segment['ArrivalAirport']['LocationCode'];
+                        $this->data['html'] .= '<p>Departure: <b>'.$departureAirportMAC.'</b>';
+                        $this->data['html'] .= ' &rarr; Arrival: <b>'.$arrivalAirportMAC.'</b></p>';
+                    }
+                    $this->data['html'] .= '</div>';
+                }
+                $airItineraryPricingInfo = $itinerary['AirItineraryPricingInfo'];
+                $itinTotalFare = $airItineraryPricingInfo['ItinTotalFare']['TotalFare'];
+                $this->data['html'] .= 'Fare: <b>'.$itinTotalFare['Amount'].'</b>';
+
+                $this->data['html'] .= '</div>';
+            }
+        }
     }
 
     protected function showTree($tree = array()){
@@ -91,5 +122,47 @@ class SabreController extends BaseController
             }
             $this->data['html'] .= '</ul>';
         }
+    }
+
+    protected function getMACs($mac = '')
+    {
+        $provider  = Request::getProvider();
+
+        $provider->setBaseUri('https://api.test.sabre.com');
+        $provider->header->set('Accept', 'application/json');
+        $provider->header->set('Authorization', 'Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/ACPCRTD!ICESMSLB\/CRT.LB!-3537010813656309855!1301187!0!!E2E-1');
+
+        $response = $provider->get('/v1/lists/supported/cities/'.$mac.'/airports/');
+
+        $macs = json_decode($response->body, true);
+
+        var_dump($macs);
+        die;
+
+        return $macs;
+    }
+
+    protected function getToken()
+    {
+        $clientId = 'V1:dgbpi3925kynqko3:DEVCENTER:EXT';
+        $clientSecret = 'H2VH9van';
+
+        $encoded = base64_encode(base64_encode($clientId).':'.base64_encode($clientSecret));
+
+        $provider  = Request::getProvider();
+
+        $provider->setBaseUri('https://api.test.sabre.com');
+
+        $provider->header->set('Authorization', 'Basic '.$encoded);
+        $provider->header->set('Content-Type', 'application/x-www-form-urlencoded');
+
+        $response = $provider->post('/v1/auth/token', array(
+            'grant_type'=>'client_credentials'
+        ));
+
+        $response = json_decode($response->body, true);
+
+        var_dump($response);
+        die;
     }
 }
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -17,11 +17,37 @@ function showFlights(){
                 document.location.assign(response.uri);
             }else if(response.html){
                 resultContainer.html(response.html);
+            }
+        },
+        url: '/sabre/show/flights',
+        dataType:  'json'
+    };
+
+    form.ajaxSubmit(options);
+
+    return false;
+}
+
+function showFlightsSource(){
+
+    var container = $('.flights.form');
+    var form = container.find('form');
+    var resultContainer = container.find('.result');
+
+    var options = {
+        success: function (response){
+
+            if(response.message){
+                alert(response.message);
+            }else if(response.uri){
+                document.location.assign(response.uri);
+            }else if(response.html){
+                resultContainer.html(response.html);
 
                 initUlHighlight('.flights ul');
             }
         },
-        url: '/sabre/show/flights',
+        url: '/sabre/source/flights',
         dataType:  'json'
     };
 
